8be6d8bc984d47243b017fef1d3dcd30951638d0
aktrooper9 <54760319+OceanNut420@users.noreply.github.com>
2023-08-11 18:24:55 -0700

    almostworks

--- src/_02_Intro_To_Searching_Algorithms/_01_LinearSearch.java
+++ src/_02_Intro_To_Searching_Algorithms/_01_LinearSearch.java
@@ -12,6 +12,17 @@ public class _01_LinearSearch {
         //    where the value was found.
         //    If the value is not found in the array, return -1.
 
-        return 0;
+    	for(int i=0;i<words.length;i++) {
+    		if(words[i] == value) {
+    			return i;
+    		}
+    	}
+    	
+    	// l1 i! a@ e3 s$ o0 +t c( l i a e s o t c
+    	// c t o s e a i l
+    	// 1!3 +0@$+ $@+ (@+ $00+ +03$ $@!1 $3@ 0!1    	
+  
+    	    	    	
+        return -1;
     }
 }
